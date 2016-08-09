70f88c2a4028ad047657f79d95c3d85ac780970d
Edrick de Guzman <edrick@ualberta.ca>
2016-08-09 03:45:08 -0600

    Add project files.

---
+++ SimpleCalculator/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace SimpleCalculator
+{
+    partial class SimpleCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstValueTextBox = new System.Windows.Forms.TextBox();
+            this.secondValueTextBox = new System.Windows.Forms.TextBox();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.addButton = new System.Windows.Forms.Button();
+            this.subtractButton = new System.Windows.Forms.Button();
+            this.multiplyButton = new System.Windows.Forms.Button();
+            this.divideButton = new System.Windows.Forms.Button();
+            this.errorDisplay = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // firstValueTextBox
+            // 
+            this.firstValueTextBox.Location = new System.Drawing.Point(80, 36);
+            this.firstValueTextBox.Name = "firstValueTextBox";
+            this.firstValueTextBox.Size = new System.Drawing.Size(102, 20);
+            this.firstValueTextBox.TabIndex = 0;
+            // 
+            // secondValueTextBox
+            // 
+            this.secondValueTextBox.Location = new System.Drawing.Point(80, 62);
+            this.secondValueTextBox.Name = "secondValueTextBox";
+            this.secondValueTextBox.Size = new System.Drawing.Size(102, 20);
+            this.secondValueTextBox.TabIndex = 1;
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(80, 88);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.ReadOnly = true;
+            this.resultTextBox.Size = new System.Drawing.Size(102, 20);
+            this.resultTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Value 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Value 2\r\n";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Result";
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(199, 26);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 39);
+            this.addButton.TabIndex = 6;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // subtractButton
+            // 
+            this.subtractButton.Location = new System.Drawing.Point(199, 71);
+            this.subtractButton.Name = "subtractButton";
+            this.subtractButton.Size = new System.Drawing.Size(75, 39);
+            this.subtractButton.TabIndex = 7;
+            this.subtractButton.Text = "Subtract";
+            this.subtractButton.UseVisualStyleBackColor = true;
+            this.subtractButton.Click += new System.EventHandler(this.subtractButton_Click);
+            // 
+            // multiplyButton
+            // 
+            this.multiplyButton.Location = new System.Drawing.Point(280, 26);
+            this.multiplyButton.Name = "multiplyButton";
+            this.multiplyButton.Size = new System.Drawing.Size(75, 39);
+            this.multiplyButton.TabIndex = 8;
+            this.multiplyButton.Text = "Multiply";
+            this.multiplyButton.UseVisualStyleBackColor = true;
+            this.multiplyButton.Click += new System.EventHandler(this.multiplyButton_Click);
+            // 
+            // divideButton
+            // 
+            this.divideButton.Location = new System.Drawing.Point(280, 71);
+            this.divideButton.Name = "divideButton";
+            this.divideButton.Size = new System.Drawing.Size(75, 39);
+            this.divideButton.TabIndex = 9;
+            this.divideButton.Text = "Divide";
+            this.divideButton.UseVisualStyleBackColor = true;
+            this.divideButton.Click += new System.EventHandler(this.divideButton_Click);
+            // 
+            // errorDisplay
+            // 
+            this.errorDisplay.AutoSize = true;
+            this.errorDisplay.Location = new System.Drawing.Point(77, 111);
+            this.errorDisplay.Name = "errorDisplay";
+            this.errorDisplay.Size = new System.Drawing.Size(0, 13);
+            this.errorDisplay.TabIndex = 10;
+            this.errorDisplay.Visible = false;
+            // 
+            // SimpleCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 148);
+            this.Controls.Add(this.errorDisplay);
+            this.Controls.Add(this.divideButton);
+            this.Controls.Add(this.multiplyButton);
+            this.Controls.Add(this.subtractButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.secondValueTextBox);
+            this.Controls.Add(this.firstValueTextBox);
+            this.Name = "SimpleCalculator";
+            this.Text = "Simple Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox firstValueTextBox;
+        private System.Windows.Forms.TextBox secondValueTextBox;
+        private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button subtractButton;
+        private System.Windows.Forms.Button multiplyButton;
+        private System.Windows.Forms.Button divideButton;
+        private System.Windows.Forms.Label errorDisplay;
+    }
+}
+
---
+++ SimpleCalculator/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCalculator
+{
+    public partial class SimpleCalculator : Form
+    {
+
+        private MyCalculator mc;
+        public SimpleCalculator()
+        {
+            InitializeComponent();
+            mc = new MyCalculator();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            //try
+            //{
+            //    mc.FirstValue = Convert.ToDouble(firstValueTextBox.Text);
+            //    mc.SecondValue = Convert.ToDouble(secondValueTextBox.Text);
+            //} catch (FormatException)
+            //{
+            //    errorDisplay.Text = "Please enter Numberical Values Only!";
+            //    errorDisplay.Visible = true;
+            //} finally
+            //{
+            //    mc.FirstValue = 0;
+            //    mc.SecondValue = 0;
+            //    errorDisplay.Visible = false;
+            //}
+
+            mc.FirstValue = Convert.ToDouble(firstValueTextBox.Text);
+            mc.SecondValue = Convert.ToDouble(secondValueTextBox.Text);
+            resultTextBox.Text = mc.Add().ToString("F");
+        }
+
+        private void subtractButton_Click(object sender, EventArgs e)
+        {
+            mc.FirstValue = Convert.ToDouble(firstValueTextBox.Text);
+            mc.SecondValue = Convert.ToDouble(secondValueTextBox.Text);
+            resultTextBox.Text = mc.Subtract().ToString("F");
+        }
+
+        private void multiplyButton_Click(object sender, EventArgs e)
+        {
+            mc.FirstValue = Convert.ToDouble(firstValueTextBox.Text);
+            mc.SecondValue = Convert.ToDouble(secondValueTextBox.Text);
+            resultTextBox.Text = mc.Multiply().ToString("F");
+        }
+
+        private void divideButton_Click(object sender, EventArgs e)
+        {
+            mc.FirstValue = Convert.ToDouble(firstValueTextBox.Text);
+            mc.SecondValue = Convert.ToDouble(secondValueTextBox.Text);
+            resultTextBox.Text = mc.Divide().ToString("F");
+
+        }
+    }
+
+    public class MyCalculator
+    {
+
+        public double FirstValue { get; set; }
+        public double SecondValue { get; set; }
+
+        public double Add()
+        {
+            return (FirstValue + SecondValue);
+        }
+
+        public double Subtract()
+        {
+            return (FirstValue - SecondValue);
+        }
+
+        public double Multiply()
+        {
+            return (FirstValue * SecondValue);
+        }
+
+        public double Divide()
+        {
+            return (FirstValue / SecondValue);
+        }
+    }
+}
